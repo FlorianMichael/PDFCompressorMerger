231b71e0a03f8766091cc06d1b656af783ca72b8
FlorianMichael <git@florianmichael.de>
2025-12-28 23:54:03 +0100

    PDFCompressorMerger 1.0.1: Prepare for next year (2026)

--- src/main/java/de/florianmichael/pdfcompressormerger/Main.java
+++ src/main/java/de/florianmichael/pdfcompressormerger/Main.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of PDFCompressorMerger - https://github.com/FlorianMichael/PDFCompressorMerger
- * Copyright (C) 2025 FlorianMichael/EnZaXD <florian.michael07@gmail.com> and contributors
+ * Copyright (C) 2025-2026 FlorianMichael/EnZaXD <git@florianmichael.de> and contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
